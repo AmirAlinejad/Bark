5dade346feded2397043a4433f3c53c4154e86bb
Sam Peters <112666557+sbpete@users.noreply.github.com>
2024-01-04 23:23:46 -0500

    Added macros file

---
+++ macros/macros.js
@@ -0,0 +1,9 @@
+export const clubCategories = [
+    {key:'1', value:'Technology'},
+    {key:'2', value:'Service/Volunteering'},
+    {key:'3', value:'Sports'},
+    {key:'4', value:'Math'},
+    {key:'5', value:'Religion'},
+    {key:'6', value:'Art'},
+    {key:'7', value:'Cultural'},
+];
