cef0144f788d65a11892e8cd455272e624f6c04b
AmirAlinejad <Amir2004>
2024-01-10 23:01:05 -0500

    fix bug

--- screens/club/ClubList.js
+++ screens/club/ClubList.js
@@ -1,66 +1,56 @@
 import React, { useState, useEffect } from 'react';
-// react native components
 import { View, StyleSheet, ScrollView } from 'react-native';
 import { Text, IconButton } from 'react-native-paper';
-// backend
 import { db } from '../../backend/FirebaseConfig';
 import { ref, onValue } from "firebase/database";
-// my components
 import ClubCategory from '../../components/clubs/ClubCategory';
 import Header from '../../components/Header';
-// macros
 import { clubCategories } from '../../macros/macros';
-// fonts
 import { title } from '../../styles/fontstyles';
 
-// club list screen
 const ClubList = ({ navigation }) => {
-  // state for club data
   const [clubData, setClubData] = useState([]);
 
-  // go to add club screen
   const onAddClubPress = () => {
     navigation.navigate("NewClub");
   }
 
-  // get data from firebase
-  useEffect (() => {
+  useEffect(() => {
     const starCountRef = ref(db, 'clubs/');
     onValue(starCountRef, (snapshot) => {
       const data = snapshot.val();
       const newClubs = Object.keys(data).map(key => ({
-        id:key, 
+        id: key,
         ...data[key]
       }));
       console.log(newClubs);
       setClubData(newClubs);
     })
-  }, [])
+  }, []);
 
-  // sort the clubs by category
-  sortedClubs = clubCategories.map((category) => {
-    return ({
+  const sortedClubs = clubCategories.map((category) => {
+    return {
       categoryName: category.value,
-      data: clubData.filter((club) => club.clubCategories.includes(category.value))
-    });
+      data: clubData.filter((club) => club.clubCategories && club.clubCategories.includes(category.value))
+    };
   });
 
   return (
     <View style={styles.container}>
       <Header text='Club List'></Header>
       <ScrollView style={styles.clubCategories}>
-      { 
-        // create a club category component for each category
-        sortedClubs.map((category) => {
-          if (category.data.length != 0) {
-            return (
-              <ClubCategory name={category.categoryName} data={category.data} navigation={navigation} />
-            )
-          }
-        })
-      }
+        {
+          sortedClubs.map((category) => {
+            if (category.data && category.data.length !== 0) {
+              return (
+                <ClubCategory key={category.categoryName} name={category.categoryName} data={category.data} navigation={navigation} />
+              )
+            } else {
+              return null; // or handle empty category case if needed
+            }
+          })
+        }
       </ScrollView>
-      {/* Add event button */}
       <View style={styles.addClubButton}>
         <IconButton
           onPress={onAddClubPress}
