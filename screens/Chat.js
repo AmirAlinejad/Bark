e7e5cba02a5145afa945a03eed61992f735a22c6
AmirAlinejad <85849136+AmirAlinejad@users.noreply.github.com>
2024-01-21 17:29:36 -0500

    Update Chat.js
    group chats

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,105 +1,111 @@
-// Chat.js
-
 import React, { useState, useEffect, useLayoutEffect, useCallback } from 'react';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
 import {
   collection,
   addDoc,
   orderBy,
   query,
-  onSnapshot
+  onSnapshot,
+  where
 } from 'firebase/firestore';
 import { signOut } from 'firebase/auth';
-import { auth,firestore, db } from '../backend/FirebaseConfig';
+import { auth, firestore, db } from './../backend/FirebaseConfig';
 import { useNavigation } from '@react-navigation/native';
 import { AntDesign } from '@expo/vector-icons';
-import colors from '../styles/Colors';
-import { View } from 'react-native';
+import colors from '../styles/colors';
 import { IconButton } from 'react-native-paper';
 
-export default function Chat() {
+export default function Chat({ route }) {
   const [messages, setMessages] = useState([]);
   const navigation = useNavigation();
+  const clubName = route?.params?.clubName;
 
   const onSignOut = () => {
     signOut(auth).catch(error => console.log('Error logging out: ', error));
   };
+
   const onBackPress = () => {
     navigation.goBack();
-  }
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <TouchableOpacity
-          style={{ marginRight: 10 }}
-          onPress={onSignOut}
-        >
-          <AntDesign name="logout" size={24} color={colors.gray} />
-        </TouchableOpacity>
-      )
-    });
-  }, [navigation]);
+  };
 
   useLayoutEffect(() => {
-    const collectionRef = collection(firestore, 'chats');
-    const q = query(collectionRef, orderBy('createdAt', 'desc'));
-  
-    const unsubscribe = onSnapshot(q, querySnapshot => {
-      console.log('querySnapshot unsubscribe');
-      setMessages(
-        querySnapshot.docs.map(doc => ({
-          _id: doc.id,
-          createdAt: doc.data().createdAt.toDate(),
-          text: doc.data().text,
-          user: doc.data().user
-        }))
+    console.log('ClubName in useLayoutEffect:', clubName);
+
+    let unsubscribe;
+
+    if (clubName) {
+      const collectionRef = collection(firestore, 'chats');
+      const q = query(
+        collectionRef,
+        where('clubName', '==', clubName),
+        orderBy('createdAt', 'desc')
       );
-    });
-  
-    return () => unsubscribe();
-  }, [db]); // Ensure db is included in the dependency array if it's being used inside the hook
-  
+
+      unsubscribe = onSnapshot(q, querySnapshot => {
+        console.log('querySnapshot in useLayoutEffect:', querySnapshot);
+        setMessages(
+          querySnapshot.docs.map(doc => ({
+            _id: doc.id,
+            createdAt: doc.data().createdAt.toDate(),
+            text: doc.data().text,
+            user: doc.data().user
+          }))
+        );
+      });
+    }
+
+    return () => {
+      if (unsubscribe) {
+        console.log('Unsubscribing...');
+        unsubscribe();
+      }
+    };
+  }, [clubName, db]);
+
   const onSend = useCallback((messages = []) => {
     setMessages(previousMessages =>
       GiftedChat.append(previousMessages, messages)
     );
 
     const { _id, createdAt, text, user } = messages[0];
-    addDoc(collection(firestore, 'chats'), {
-      _id,
-      createdAt,
-      text,
-      user
-    });
-  }, []);
+    if (clubName) {
+      addDoc(collection(firestore, 'chats'), {
+        _id,
+        createdAt,
+        text,
+        user,
+        clubName
+      });
+    }
+  }, [clubName]);
 
   return (
     <View style={{ flex: 1 }}>
-    <View style={{ marginTop: 30, paddingHorizontal: 10 }}>
-      <IconButton
-        onPress={onBackPress}
-        icon="arrow-left"
-        size={30}
+      <View style={{ marginTop: 30, paddingHorizontal: 10 }}>
+        <IconButton
+          onPress={onBackPress}
+          icon="arrow-left"
+          size={30}
+        />
+      </View>
+      <GiftedChat
+        messages={messages}
+        showAvatarForEveryMessage={false}
+        showUserAvatar={false}
+        onSend={messages => onSend(messages)}
+        messagesContainerStyle={{
+          backgroundColor: '#fff'
+        }}
+        textInputStyle={{
+          backgroundColor: '#fff',
+          borderRadius: 20,
+        }}
+        user={{
+          _id: auth?.currentUser?.email,
+          avatar: 'https://i.pravatar.cc/300'
+        }}
       />
     </View>
-    <GiftedChat
-      messages={messages}
-      showAvatarForEveryMessage={false}
-      showUserAvatar={false}
-      onSend={messages => onSend(messages)}
-      messagesContainerStyle={{
-        backgroundColor: '#fff'
-      }}
-      textInputStyle={{
-        backgroundColor: '#fff',
-        borderRadius: 20,
-      }}
-      user={{
-        _id: auth?.currentUser?.email,
-        avatar: 'https://i.pravatar.cc/300'
-      }}
-    />
-  </View>
   );
 }
